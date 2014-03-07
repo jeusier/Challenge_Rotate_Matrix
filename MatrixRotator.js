13ca52f8b354a2f0a010bcc03a55a5c413531a5d
jahsan87 <jahsan87@gmail.com>
2014-03-07 11:53:04 -1000

    refactored to accept any size matrix, created new tests, passed all tests

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -23,6 +23,8 @@ MatrixRotator.prototype.rotate = function(direction) {
   
   //store matrix length
   matrixLength = this.matrix.length;
+  console.log("length: "+matrixLength);
+  tempArray = new Array();
 
   //create new temp matrix
   rotatedMatrix = new Array();
@@ -33,33 +35,34 @@ MatrixRotator.prototype.rotate = function(direction) {
   //runs if direction is clockwise
   if (direction === Direction.CW) {
     //loop through original matrix to build each array in the temp matrix
+    //i is the index of the element of the inner array in the original matrix
     for (var i = 0; i < matrixLength; i++) {
-        //push an array that has each element with decending indexes of the original matrix
-        //while the inner array's index remains the same until next loop where it increments
-        rotatedMatrix.push([
-            this.matrix[matrixLength-1][i],
-            this.matrix[matrixLength-2][i],
-            this.matrix[matrixLength-3][i],
-            this.matrix[matrixLength-4][i],
-            this.matrix[matrixLength-5][i]
-        ]);
+        //create an array to be placed into a temp array
+        //j is the index of the outer array in the matrix
+        for (var j = matrixLength-1; j >= 0; j--) {
+            tempArray.push(this.matrix[j][i]);
+        }
+        //push temp array into rotated matrix and reset temp array
+        rotatedMatrix.push(tempArray);
+        tempArray = [];
 
     } 
+    console.log("rotate: "+rotatedMatrix);
   }
 
   //runs if direction is counterclockwise
   if (direction === Direction.CCW) {
     //loop through original matrix to build each array in the temp matrix
+    //i is the index of the element of the inner array in the original matrix
     for (var i = matrixLength-1; i >= 0; i--) {
-        //push an array that has each element with ascending indexes of the matrix
-        //while the inner array's index remains the same until next loop where it decrements
-        rotatedMatrix.push([
-            this.matrix[matrixLength-5][i],
-            this.matrix[matrixLength-4][i],
-            this.matrix[matrixLength-3][i],
-            this.matrix[matrixLength-2][i],
-            this.matrix[matrixLength-1][i]
-        ]);
+        //create an array to be placed into a temp array
+        //j is the index of the outer array in the matrix
+        for (var j = 0; j < matrixLength; j++) {
+            tempArray.push(this.matrix[j][i]);
+        }
+        //push temp array into rotated matrix and reset temp array
+        rotatedMatrix.push(tempArray);
+        tempArray = [];
 
     } 
 
--- spec/MatrixRotator-spec.js
+++ spec/MatrixRotator-spec.js
@@ -5,6 +5,7 @@ var Direction = require("../Direction").Direction;
 describe("The Matrix Rotator", function () {
   
   var matrixRotator = new MatrixRotator(Matrix.matrix1);
+  var matrixRotator2 = new MatrixRotator(Matrix.matrix2);
   
   it("can rotate Clockwise", function () {
     matrixRotator.rotate(Direction.CW);
@@ -29,5 +30,32 @@ describe("The Matrix Rotator", function () {
                                             [8,4,3,0,9]
                                           ]);
   });
+
+  it("can rotate Clockwise", function () {
+    matrixRotator2.rotate(Direction.CW);
+    expect(matrixRotator2.matrix).toEqual( [
+                                            [6,0,1,3,4,0],
+                                            [9,9,8,7,9,1],
+                                            [7,4,5,0,4,1],
+                                            [0,6,0,1,4,3],
+                                            [0,7,5,7,5,0],
+                                            [2,8,2,8,7,5]
+                                          ]);
+
+  });
+
+  it("can rotate CounterClockwise", function () {
+    matrixRotator2.rotate(Direction.CCW);
+    matrixRotator2.rotate(Direction.CCW);
+    expect(matrixRotator2.matrix).toEqual([
+                                            [5,7,8,2,8,2],
+                                            [0,5,7,5,7,0],
+                                            [3,4,1,0,6,0],
+                                            [1,4,0,5,4,7],
+                                            [1,9,7,8,9,9],
+                                            [0,4,3,1,0,6]
+                                          ]);
+  });
+
 
 });    
