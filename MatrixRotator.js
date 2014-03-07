4c925b2e5ae979eb516f5147994a02f3940dfaa0
jahsan87 <jahsan87@gmail.com>
2014-03-07 11:31:08 -1000

    completed both tests

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -20,8 +20,55 @@ function MatrixRotator(matrix){
 //                                         v        or Direction.CCW
 MatrixRotator.prototype.rotate = function(direction) {
   // do work here
+  
+  //store matrix length
+  matrixLength = this.matrix.length;
+
+  //create new temp matrix
+  rotatedMatrix = new Array();
+
 
   // must be a valid Direction, see Direction.js
+
+  //runs if direction is clockwise
+  if (direction === Direction.CW) {
+    //loop through original matrix to build each array in the temp matrix
+    for (var i = 0; i < matrixLength; i++) {
+        //push an array that has each element with decending indexes of the original matrix
+        //while the inner array's index remains the same until next loop where it increments
+        rotatedMatrix.push([
+            this.matrix[matrixLength-1][i],
+            this.matrix[matrixLength-2][i],
+            this.matrix[matrixLength-3][i],
+            this.matrix[matrixLength-4][i],
+            this.matrix[matrixLength-5][i]
+        ]);
+
+    } 
+  }
+
+  //runs if direction is counterclockwise
+  if (direction === Direction.CCW) {
+    //loop through original matrix to build each array in the temp matrix
+    for (var i = matrixLength-1; i >= 0; i--) {
+        //push an array that has each element with ascending indexes of the matrix
+        //while the inner array's index remains the same until next loop where it decrements
+        rotatedMatrix.push([
+            this.matrix[matrixLength-5][i],
+            this.matrix[matrixLength-4][i],
+            this.matrix[matrixLength-3][i],
+            this.matrix[matrixLength-2][i],
+            this.matrix[matrixLength-1][i]
+        ]);
+
+    } 
+
+
+  }
+    
+    //rewrite original matrix with rotated matrix
+    this.matrix = rotatedMatrix;
+
 
 
 };
